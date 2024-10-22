d21a9b0dbd4db0aaf03ae923ed8542e29bf77b9a
GIJoelX <gecfghacksy@gmail.com>
2024-10-22 02:06:24 +0200

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
 class ExampleProvider(val plugin: TestPlugin) : MainAPI() { // all providers must be an intstance of MainAPI
-    override var mainUrl = "https://example.com/" 
+    override var mainUrl = "https://api.movie2k.ch" 
     override var name = "Example provider"
     override val supportedTypes = setOf(TvType.Movie)
 
